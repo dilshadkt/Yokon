4c091ca1d3179b5b3cd2ab3c4910241d552a9a76
Dilshad <hmydilshadkt@gmail.com>
2024-02-10 19:38:00 +0530

    fix:bg issure in why chooses

--- src/components/WhychooseCard.jsx
+++ src/components/WhychooseCard.jsx
@@ -3,9 +3,8 @@ import React from "react";
 const WhychooseCard = ({ image, title, description, index }) => {
   return (
     <div
-      className={`flex items-center justify-center flex-col p-4 text-white  font-[forum] bg-[${
-        index % 2 === 0 ? `#1C1C1C` : `#0F0F0F`
-      }] `}
+      style={{ background: `${index % 2 === 0 ? `#1C1C1C` : `#0F0F0F`}` }}
+      className={`flex items-center justify-center flex-col p-4 text-white  font-[forum]  `}
     >
       <div className="w-[75px] my-4">
         <img src={image} alt={image} />
